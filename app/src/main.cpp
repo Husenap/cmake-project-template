9e9dab477e4553128208fee063cd74b17a5bd56c
Hussein Taher <hussein_taher@outlook.com>
2020-06-13 01:48:40 +0200

    remove glfw

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,31 +1,7 @@
-﻿#include <GLFW/glfw3.h>
-
-#include <iostream>
-
-void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-}
+﻿#include <iostream>
 
 int main() {
-	if (!glfwInit()) {
-		return 1;
-	}
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "cmake+conan project template", nullptr, nullptr);
-
-	if (!window) {
-		return 1;
-	}
-
-	glfwSetKeyCallback(window, KeyCallback);
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
+	std::cout << "Hello World!" << std::endl;
 
 	return 0;
 }
