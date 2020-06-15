2b4dfdcb3ac624ea73e34ee7a0d015860f34179c
Hussein Taher <hussein_taher@outlook.com>
2020-06-15 21:39:37 +0200

    updated compiler flag for gcc

--- cmake/compiler_features.cmake
+++ cmake/compiler_features.cmake
@@ -1,7 +1,7 @@
 if (MSVC)
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest")
 else()
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++2a")
 endif()
 
 message("-- CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
