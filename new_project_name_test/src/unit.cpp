1f3349ffb2a4d70f02bbf1b7a186abc2267ad458
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-07-09 19:34:24 +0200

    improve template (#6)
    * updated scripts
    
    * updated actions
    
    * updated clang-format
    
    * Formatted files using clang-format
    
    * updated actions
    
    * update actions
    
    * update cmake configuration
    
    * missed a project_name
    
    * updated readme
    
    * updated actions
    
    * updated actions
    
    * updated actions
    
    * updated actions
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ new_project_name_test/src/unit.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(new_project_name, example_test) {
+    EXPECT_EQ(2, 1 + 1);
+}
--- project_name_test/src/unit.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(project_name, example_test) {
-	EXPECT_EQ(2, 1 + 1);
-}
