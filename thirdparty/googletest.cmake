655388db323220bbb0eaa32a9692e8bfe824cb63
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-07-09 19:13:50 +0200

    update cmake configuration

--- thirdparty/googletest.cmake
+++ thirdparty/googletest.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY  https://github.com/google/googletest.git
-    GIT_TAG         a4ab0ab # This includes a fix when using C++20
+    GIT_TAG         release-1.11.0
 )
 
 FetchContent_MakeAvailable(googletest)
